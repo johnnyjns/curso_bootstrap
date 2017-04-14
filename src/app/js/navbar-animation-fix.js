2f4b7b71c1469b8c3553b9ca9f86e29a8521c1e6
Johnny Souto <johnnyjns@gmail.com>
2017-04-14 00:33:05 -0300

    fim do menu navbar

--- src/app/js/navbar-animation-fix.js
+++ src/app/js/navbar-animation-fix.js
@@ -1,1 +1,7 @@
-$('.topCasaFina-banner')
+$('#collapsed-navbar').on('show.bs.collapse', function () {
+  $('.topCasaFina-banner').css('transform', 'translate( -50%, 10%)');
+});
+
+$('#collapsed-navbar').on('hidden.bs.collapse', function () {
+  $('.topCasaFina-banner').css('transform', 'translate( -50%, -50%)');
+});
